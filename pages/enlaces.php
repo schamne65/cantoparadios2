8818102163ddcdf8e98e51e9832eef1eb29cdb31
unknown <schamne65@gmail.com>
2024-07-26 12:57:10 -0300

    enlaces

--- pages/enlaces.php
+++ pages/enlaces.php
@@ -1,11 +1,22 @@
 <?php
+$pageClass['class']="link";
 include '../includes/structure_top.php';
 ?>
-<div>
-    <a href="https://www.youtube.com/@valivanmta"><span class="icon-youtube"><span>Valivan</span></span></a>
-    <p>Valivan es un grupo de hermano sque atraves del arte llevan el envangelio a los mas pequeños</p>
+<div class="link-container">
+    <a href="https://www.youtube.com/@valivanmta" target="_blank"><span class="icon-youtube"><span class="link-name">Valivan</span></span></a>
+    <p>Valivan es el fruto del amor de una familia española que por medio del arte llevan el envangelio a los mas pequeños</p>
 </div>
-
+<div class="link-container">
+    <a href="https://www.youtube.com/@cantoparadios4024" target="_blank"><span class="icon-youtube"></span><span class="link-name">Canto para Dios</span></a>
+    <p>Llevando los poemas de san Juan de la Cruz y de santa Teresa de Avila al plano musical</p>
+</div>
+<div class="link-container">
+    <a href="https://www.youtube.com/@PadreLeonardoIsidoroYacob" target="_blank"><span class="icon-youtube"></span><span class="link-name">P. Leonardo I. Yacob</span></a>
+    <p>Padre Leo para muchos</p>
+</div>
+<div>
+    <img src="" alt="">
+</div>
 <?php 
 include '../includes/structure_bottom.php';
 ?>
