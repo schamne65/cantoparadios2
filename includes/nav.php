a76f81f79f4a22b633f17b54043ec68b7f308bec
unknown <schamne65@gmail.com>
2024-07-31 14:37:00 -0300

    usuario

--- includes/nav.php
+++ includes/nav.php
@@ -9,11 +9,7 @@
                 <li><a href="../pages/documentos.php">Documentos</a></li>
                 <li><a href="../pages/enlaces.php">Enlaces</a></li>
                 <?php
-                if ($_SESSION['username'] == "nicolas") {?>
-                    <li><a href="../panel/panel-documento.php">Panel documentos</a></li>
-                    <li><a href="../panel/panel-video.php">Panel videos</a></li>
-                 <?php }
-                ?>
+                 ?>
             </ul>
         </nav>
     <span><a href="../pages/usuarios/logout.php">Salir</a></span>
