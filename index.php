1d9d8d1e6c461d8fb49004b2162726afaa7afdb4
unknown <schamne65@gmail.com>
2024-07-31 10:04:18 -0300

    usuario

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 <body class="index">
     <h1>Bienvenidos a Canto para Dios</h1>
-    <a class="enlace" href="./pages/usuarios/login.php">Ingresar    </a>
+    <a class="enlace" href="./usuarios/login.php">Ingresar    </a>
     <span class="acerca_de"><a href="./pages/acercade.php">¿Queres conocer más?</a></span>
 </body>
 </html>
