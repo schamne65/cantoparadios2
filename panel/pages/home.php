eb1920e192fdd4ffdf124c44f3cca74be58f4a49
unknown <schamne65@gmail.com>
2024-07-31 06:59:44 -0300

    pruebaa

---
+++ panel/pages/home.php
@@ -0,0 +1,20 @@
+<?php
+include '../includes/structure_top.php';
+?>
+<div class="flex home">
+    <cite>
+        <p>"¡Cuánto lloré al oír vuestros himnos y cánticos, fuertemente conmovido por las voces de vuestra Iglesia, que suavemente cantaba! Entraban aquellas voces en mis oídos, y vuestra verdad se derretía en mi corazón, y con esto se inflamaba el afecto de piedad, y corrían las lágrimas, y me iba bien con ellas.!"  <span><strong>San Agustín, Confessiones 9, 6, 14</strong></span>.</p>
+    </cite>    
+    <img id="img-home" src="../img/sanagustin.jpeg" alt="Imagen del san Agustín">
+</div>
+<div class="flex home">
+
+    <cite>    
+        <p>... La composición y el canto de salmos inspirados, con frecuencia acompañados de instrumentos musicales, estaban ya estrechamente ligados a las celebraciones litúrgicas de la Antigua Alianza. La Iglesia continúa y desarrolla esta tradición: "Recitad entre vosotros salmos, himnos y cánticos inspirados; cantad y salmodiad en vuestro corazón al Señor" ...   <span><strong>C.I.C  1156</strong></span>.</p>
+    </cite>    
+    
+</div>
+
+<?php 
+include '../includes/structure_bottom.php';
+?>
